6209fc3dffdbbfe94608bbeb7b05e994ae20c042
Эдгар Неверовский <edgarneverovskij@gmail.com>
2022-01-10 18:46:40 +0200

    Не готово

--- MyModule.py
+++ MyModule.py
@@ -51,3 +51,40 @@ def alustus():
 #            else:
 #                print("Teie password sisetab ebakorrektset sümbolit või ta on liiga lühike")
 #    print(user_password)
+
+import random
+
+def randompass()->str:
+    """Пароль создаётся рандомно
+    """
+    str0=".,:;!_*-+()/#¤%&"
+    str1 = '0123456789'
+    str2 = 'qwertyuiopasdfghjklzxcvbnm'
+    str3 = str2.upper() # 'QWERTYUIOPASDFGHJKLZXCVBNM'
+    str4 = str0+str1+str2+str3
+    ls = list(str4)
+    random.shuffle(ls) #перемешиваются все символы
+    psword = ''.join([random.choice(ls) for x in range(12)])
+    return psword
+
+def control(passwords:str)->str:
+    """Проверка пароля, самое главное чтобы в личном пароле присутсвовал хотя 1 тип символов.
+    """
+    str0=".,:;!_*-+()/#¤%&"
+    alpha=digit=upper=sprcial=0
+    ls=list(str0)
+    pas=list(passwords)
+    for i in range(len(pas)):
+        if pas[i].isupper():
+            upper=1
+        if pas[i].isalpha():
+            alpha=1
+        if pas[i].isdigit():
+            digit=1
+        if pas[i] in ls:
+            special=1
+    if alpha==1 and digit==1 and upper==1 and special==1:
+        control=True
+    else:
+        control=False
+    return control
--- Vilmem.net.py
+++ Vilmem.net.py
@@ -1,3 +1,40 @@
 from MyModule import*
 alustus()
+from module1 import *
 
+users=["Denis"]
+passwords=["12345"]
+
+while True:
+    print("Зарегистрироваться-1, Авторизоваться-2, Выход-3")
+    ch=int(input())
+    if ch==1:
+        print("Вы выбрали регистрацию")
+        while 1:
+            log=input("Введите свое имя: ")
+            if log not in users:
+                print("Данное имя не было занято, теперь оно принадлежит вам.")
+                break
+            else:
+                print("Данное имя уже кем то используется.")
+        ch=input("Самим записать пароль(S) или робот придумает(R)? ")
+        if ch.upper()=="R":
+            pas=randompass()
+            print(pas)
+        elif ch.upper()=="S":
+            while 1:
+                pas=input("Введите свой пароль: ")
+                result=control(pas)
+                if result==True:
+                    users.append(log)
+                    passwords.append(pas)
+                    break
+    elif ch==2: 
+        print("Вы выбрали авторизацию")
+        user=input("Введите свое имя: ")
+        pas=input("Введите свой пароль: ")
+        if users.index(user)==passwords.index(pas):
+            print("Добро пожаловать")
+    elif ch==3:
+        print("Выход из программы")
+        break
